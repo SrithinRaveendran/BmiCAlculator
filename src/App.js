ce154d4295b5c142e2690fd4e769001cc1d2b05c
SrithinRaveendran <srithinraveendran@gmail.com>
2025-08-13 16:14:31 +0530

    'bmi'

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import BmiCalculator from './components/Bmi'
+
+//import {GlobalStyle} from './styledComponents'
+
+const App = () => (
+  <>
+    
+    <BmiCalculator />
+  </>
+)
+
+export default App
